3c30effe66fa8d7ac7ae60bd3585e97b0a6c83eb
EduardoHillebrand <EduardoHillebrand@users.noreply.github.com>
2025-10-09 23:46:21 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "placehold.co" },
       { protocol: "http", hostname: "localhost", port: "4000" },
       { protocol: "http", hostname: "127.0.0.1", port: "4000" },
-      { protocol: "https", hostname: "app.bibliotecaespirita.ong.br", port: "80" },
+      { protocol: "https", hostname: "api.bibliotecaespirita.ong.br", port: "80" },
       { protocol: "https", hostname: "bibliotecaespirita.ong.br", port: "80" }
     ]
   },
