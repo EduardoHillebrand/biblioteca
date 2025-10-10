320006ae7b8761e36225c474b6e69518151b74ea
EduardoHillebrand <EduardoHillebrand@users.noreply.github.com>
2025-10-09 23:49:34 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
-      { protocol: "http", hostname: "localhost", port: "4000" },
-      { protocol: "http", hostname: "127.0.0.1", port: "4000" },
-      { protocol: "https", hostname: "api.bibliotecaespirita.ong.br", port: "80" },
-      { protocol: "https", hostname: "bibliotecaespirita.ong.br", port: "80" }
+      { protocol: "https", hostname: "api.bibliotecaespirita.ong.br", port: "80" }
     ]
   },
   eslint: { ignoreDuringBuilds: true },
